0d1c72edd2573638f1993d58015fe982768f7aea
pleaw <whtkdgml36271@gmail.com>
2023-10-23 10:28:56 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/example/jpanote/JpanoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpanoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpanoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpanote/JpanoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpanoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
