b0e4951efa9e1da303e19cd40483d64e729dd3d3
pleajsh <whtkdgml36271@gmail.com>
2023-12-01 17:25:07 +0900

    websocket 적용

---
+++ src/main/java/com/example/jpanote/websocket/service/WebSocketService.java
@@ -0,0 +1,17 @@
+package com.example.jpanote.websocket.service;
+
+/**
+ * Version     : 1.0
+ * 패키지명    　: com.example.jpanote.websocket.service
+ * 파일명    　　: WebSocketService
+ * 작성일자    　: 2023-11-27
+ * 작성자    　　: 조상희
+ * 설명    　　　: 클래스 설명
+ * 수정일자    　: *
+ * 수정자    　　: *
+ * 수정내역    　: *
+ */
+
+
+public class WebSocketService {
+}
